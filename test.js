52c121dafd68faae3b03d5f4a4c23e0ec8e9e83a
varad4545 <varad4545@users.noreply.github.com>
2025-05-09 11:47:57 +0000

    security: Fix 1. Cross-Site Scripting (XSS) vulnerability in test.js

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
-//test-comment
 app.get('/search', (req, res) => {
     const query = req.query.q;
     res.send(`
-      <h1>Search Results for: ${query}</h1>
+      <h1>Search Results for: ${escape(query)}</h1>
       <div id="results">...</div>
     `);
   });
-  
+
+function escape(str) {
+    return str
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
